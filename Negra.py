836aeddb5882efbf89c20e919a6dd9f3e88aa9ba
DeathKira <4E657F6G@users.noreply.github.com>
2018-05-08 17:52:41 -0600

    Update Negra.py

--- Negra.py
+++ Negra.py
@@ -1,5 +1,14 @@
 import Ficha
 
 
-class Negra(Ficha):
-    pass
+class Negra(Ficha.Ficha):
+    def __init__(self, imagen, color, posimg):
+        self.imagen = imagen
+        self.color = color
+        self.posimg = posimg
+
+    def cargarimg(self, imagen):
+        super().cargarimg(imagen)
+
+    def blitimg(self, imagen, posimg):
+        super().blitimg(imagen, posimg)
