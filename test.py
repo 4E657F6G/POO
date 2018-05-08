4fc5d2870b1028a58b80dff64f345db9ecf85f4c
DeathKira <4E657F6G@users.noreply.github.com>
2018-05-08 16:25:16 -0600

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 pygame.init()
 
 pantalla = pygame.display.set_mode((620, 540), 0, 32)
-imagen = pygame.image.load("img/ficha.jpg")
+imagen = pygame.image.load("img/ficha.jpg").convert()
 
 pos = [10, 10]
 
@@ -14,10 +14,9 @@ while True:
         if eventos.type == pygame.QUIT:
             exit()
         elif eventos.type == pygame.MOUSEBUTTONUP:
+            reloj.tick(10)
+            pantalla.fill((0, 0, 0))
             pantalla.blit(imagen, pos)
+            pos = pygame.mouse.get_pos()
 
-    reloj.tick(10)
-    pantalla.fill((0, 0, 0))
-    pantalla.blit(imagen, pos)
-    pos = pygame.mouse.get_pos()
     pygame.display.update()
