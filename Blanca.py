2af7d024a076882502331c4be3f27009f2baabd3
DeathKira <4E657F6G@users.noreply.github.com>
2018-05-08 17:53:07 -0600

    Update Blanca.py

--- Blanca.py
+++ Blanca.py
@@ -2,4 +2,13 @@ import Ficha
 
 
 class Blanca(Ficha):
-    pass
+    def __init__(self, imagen, color, posimg):
+        self.imagen = imagen
+        self.color = color
+        self.posimg = posimg
+
+    def cargarimg(self, imagen):
+        super().cargarimg(imagen)
+
+    def blitimg(self, imagen, posimg):
+        super().blitimg(imagen, posimg)
