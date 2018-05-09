951639dcff9d971704adeefff97bf5a87c283894
DeathKira <4E657F6G@users.noreply.github.com>
2018-05-08 19:50:45 -0600

    Update Ficha.py

--- Ficha.py
+++ Ficha.py
@@ -3,19 +3,19 @@ import pygame
 import Tablero
 
 
-class Ficha(metaclass=abc.ABCMeta, Tablero):
+class Ficha(metaclass=abc.ABCMeta, Tablero.Tablero):
     def __init__(self, color, tipoficha, img):
         self.color = color
         self.tipoficha = tipoficha
         self.img = img
-    
+
     @abc.abstractmethod
     def settipoficha(self, tipo):
         self.tipoficha = tipo
-    
+
     def gettipoficha(self):
-        return self.tipoficha    
-    
+        return self.tipoficha
+
     @abc.abstractmethod
     def setcolor(self, color):
         pass
@@ -30,4 +30,4 @@ class Ficha(metaclass=abc.ABCMeta, Tablero):
 
     @abc.abstractmethod
     def blitimg(self, imagen, posimg):
-        Tablero.pantalla().blit(imagen, posimg)
+        Tablero.Tablero.pantalla.blit(imagen, posimg)
